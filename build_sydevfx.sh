ad0fb509d0dfa5502e2d3d2f515bcfa8e7feb2d4
Blaque Allen <sydelink@gmail.com>
2020-12-17 22:49:01 -0600

    Added build option

--- build_sydevfx.sh
+++ build_sydevfx.sh
@@ -1,3 +1,7 @@
  #!/bin/bash
 
-./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=yes KERNEL_ONLY=no KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sydevfx
+# Full Build
+#./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=yes KERNEL_ONLY=no KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sydevfx
+
+# Kernel Only
+./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=yes KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sydevfx
