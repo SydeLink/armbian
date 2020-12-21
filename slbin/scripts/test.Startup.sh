4d5b855e2e86b2f693cdc842193d536830efba9c
Blaque Allen <sydelink@gmail.com>
2020-12-20 20:41:40 -0600

    Sample scripts

--- slbin/scripts/test.Startup.sh
+++ slbin/scripts/test.Startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-RootPasswd="sydelink"
-RealName="Mastermind"
-RealUserName="mastermind"
-UserPasswd="123456"
+sHOSTNAME="sydelink-dev"
+sROOTPWD="sydelink"
+sNAME="Mastermind"
+sUSERNAME="mastermind"
+sUSERPWD="123456"
 
 function pause(){
    read -p "$*"
@@ -11,44 +12,47 @@ function pause(){
 
 SydeSystemSetup() {   # SydeLink Setup
 
-    echo ">Starting SydeSystemSetup..."
-    echo $RootPasswd
-    echo $RealName
-    echo $RealUserName
-    echo $UserPasswd
+    echo ">SydeLink Setup"
+    echo $sHOSTNAME
+    echo $sROOTPWD
+    echo $sNAME
+    echo $sUSERNAME
+    echo $sUSERPWD
+    echo $sUSERGROUPS
+    echo ""
 
     echo "-Set root password"
-	echo root:$RootPasswd | chpasswd
+	echo root:$sROOTPWD | chpasswd
 
     echo "-Adding user"
-    adduser --quiet --disabled-password --shell /bin/bash --home /home/"$RealUserName" --gecos "$RealName" "$RealUserName"
-    (echo $Password;echo $Password;) | passwd "$RealUserName" >/dev/null 2>&1
+    adduser --quiet --disabled-password --shell /bin/bash --home /home/"$sUSERNAME" --gecos "$sNAME" "$sUSERNAME"
+    (echo $sUSERPWD;echo $sUSERPWD;) | passwd "$sUSERNAME" >/dev/null 2>&1
     
     echo "-Adding user to groups"
     for additionalgroup in sudo netdev audio video disk tty users games dialout plugdev input bluetooth systemd-journal ssh adm dip pulse-access; do
-        usermod -aG ${additionalgroup} ${RealUserName} 2>/dev/null
+        usermod -aG ${additionalgroup} ${sUSERNAME} 2>/dev/null
     done
     pause 'Press [Enter] key to continue...'
 
     echo "Setting Locals"
     # setting detected locales only for user
     if [[ -n $LOCALES ]]; then
-        echo "export LC_ALL=$LOCALES" >> /home/$RealUserName/.bashrc
-        echo "export LANG=$LOCALES" >> /home/$RealUserName/.bashrc
-        echo "export LANGUAGE=$LOCALES" >> /home/$RealUserName/.bashrc
-        echo "export LC_ALL=$LOCALES" >> /home/$RealUserName/.xsessionrc
-        echo "export LANG=$LOCALES" >> /home/$RealUserName/.xsessionrc
-        echo "export LANGUAGE=$LOCALES" >> /home/$RealUserName/.xsessionrc
+        echo "export LC_ALL=$LOCALES" >> /home/$sUSERNAME/.bashrc
+        echo "export LANG=$LOCALES" >> /home/$sUSERNAME/.bashrc
+        echo "export LANGUAGE=$LOCALES" >> /home/$sUSERNAME/.bashrc
+        echo "export LC_ALL=$LOCALES" >> /home/$sUSERNAME/.xsessionrc
+        echo "export LANG=$LOCALES" >> /home/$sUSERNAME/.xsessionrc
+        echo "export LANGUAGE=$LOCALES" >> /home/$sUSERNAME/.xsessionrc
     fi
     pause 'Press [Enter] key to continue...'
 
     echo "Xenial Fix"
     # fix for gksu in Xenial
-    touch /home/$RealUserName/.Xauthority
-    chown $RealUserName:$RealUserName /home/$RealUserName/.Xauthority
-    RealName="$(awk -F":" "/^${RealUserName}:/ {print \$5}" </etc/passwd | cut -d',' -f1)"
-    [ -z "$RealName" ] && RealName=$RealUserName
-    echo -e "\nDear \e[0;92m${RealName}\x1B[0m, your account \e[0;92m${RealUserName}\x1B[0m has been created and is sudo enabled."
+    touch /home/$sUSERNAME/.Xauthority
+    chown $sUSERNAME:$sUSERNAME /home/$sUSERNAME/.Xauthority
+    RealName="$(awk -F":" "/^${sUSERNAME}:/ {print \$5}" </etc/passwd | cut -d',' -f1)"
+    [ -z "$sNAME" ] && RealName=$sUSERNAME
+    echo -e "\nDear \e[0;92m${sNAME}\x1B[0m, your account \e[0;92m${sUSERNAME}\x1B[0m has been created and is sudo enabled."
     echo -e "Please use this account for your daily work from now on.\n"
     pause 'Press [Enter] key to continue...'
 
@@ -60,9 +64,9 @@ SydeSystemSetup() {   # SydeLink Setup
     # set up profile sync daemon on desktop systems
     which psd >/dev/null 2>&1
     if [ $? -eq 0 ]; then
-        echo -e "${RealUserName} ALL=(ALL) NOPASSWD: /usr/bin/psd-overlay-helper" >> /etc/sudoers
-        touch /home/${RealUserName}/.activate_psd
-        chown $RealUserName:$RealUserName /home/${RealUserName}/.activate_psd
+        echo -e "${sUSERNAME} ALL=(ALL) NOPASSWD: /usr/bin/psd-overlay-helper" >> /etc/sudoers
+        touch /home/${sUSERNAME}/.activate_psd
+        chown $sUSERNAME:$sUSERNAME /home/${sUSERNAME}/.activate_psd
     fi
     pause 'Press [Enter] key to continue...'
 
