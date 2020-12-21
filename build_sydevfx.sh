76ec6b1302f0585acb59d2c32563b9e7cd74f2f3
Blaque Allen <sydelink@gmail.com>
2020-12-20 20:56:23 -0600

    b

--- build_sydevfx.sh
+++ build_sydevfx.sh
@@ -10,4 +10,7 @@
 #./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=yes KERNEL_ONLY=no KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sydevfx CLEAN_LEVEL=make,debs,oldcache,alldebs,images,cache,sources
 
 # CLEAN Kernel Only
-./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=yes KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sydevfx CLEAN_LEVEL=make,debs,oldcache,alldebs,images,cache,sources
+#./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=yes KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sydevfx CLEAN_LEVEL=make,debs,oldcache,alldebs,images,cache,sources
+
+# CLEAN Test-New Kernel
+./compile.sh  BOARD=rockpi-4b BRANCH=legacy RELEASE=buster BUILD_MINIMAL=no BUILD_DESKTOP=no KERNEL_ONLY=yes KERNEL_CONFIGURE=prebuilt COMPRESS_OUTPUTIMAGE=sha,gpg,img LIB_TAG=sL-stretch CLEAN_LEVEL=make,debs,oldcache,alldebs,images,cache,sources
